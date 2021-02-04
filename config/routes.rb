e5d20e99f2fb5c92be40b7a8d4e8f4f0545b8c05
Sa110-5325 <sa110.35325@gmail.com>
2021-02-04 03:42:32 +0000

    コントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'beans/index'
+  get 'beans/show'
+  get 'beans/edit'
+  get 'beans/new'
+  get 'homes/top'
+  get 'homes/about'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
