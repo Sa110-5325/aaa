337614a45a32817e3287e6930a6b90085885ff3c
Sa110-5325 <sa110.35325@gmail.com>
2021-02-04 14:15:38 +0000

    試し

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     user_path(current_user.id) # ログイン後に遷移するpathを設定
   end
+  
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
 
   protected
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get '/about' => 'homes#about'
-  
   resources :users, only: [:show, :edit]
   resources :beans, only: [:index, :show, :edit, :new, :create, :update, :destroy] do
     resource :favorites, only: [:create, :destroy]
