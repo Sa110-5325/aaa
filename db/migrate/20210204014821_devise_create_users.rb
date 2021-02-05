e69895b23cf87b45f61ed0c1eb47bc1bf854faf5
Sa110-5325 <sa110.35325@gmail.com>
2021-02-05 01:27:17 +0000

    テスト

--- db/migrate/20210204014821_devise_create_users.rb
+++ db/migrate/20210204014821_devise_create_users.rb
@@ -5,7 +5,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       ## Database authenticatable
       t.string :name
-      t.string :profile_image
+      t.string :profile_image_id
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
@@ -34,7 +34,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-      
+
       t.timestamps null: false
     end
 
