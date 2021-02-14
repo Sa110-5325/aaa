fee4c8bf8d179efdcb2c91c7c786eec732cd595d
Sa110-5325 <sa110.35325@gmail.com>
2021-02-14 08:37:29 +0000

    [add]ゲストログイン機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,15 @@ class User < ApplicationRecord
   has_many :beans, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  
+
   validates :name, presence: true
+
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.password_confirmation = user.password
+      user.name = "ゲスト"
+      # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
+    end
+  end
 end
