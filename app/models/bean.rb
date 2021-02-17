293d49c1b8a1786b1df6223aaa42d0021feb52f5
Sa110-5325 <sa110.35325@gmail.com>
2021-02-17 15:13:37 +0000

    [fix]ボタン装飾

--- app/models/bean.rb
+++ app/models/bean.rb
@@ -3,11 +3,18 @@ class Bean < ApplicationRecord
   attachment :image
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  
+
   validates :name, presence: true
+  validates :rate, presence: true
   validates :price, presence: true
   validates :info, presence: true
-  
+  validates :image, presence: true
+  validates :refresh, inclusion: { in: [1,2,3,4,5] }
+  validates :bitter, inclusion: { in: [1,2,3,4,5] }
+  validates :body, inclusion: { in: [1,2,3,4,5] }
+  validates :fruity, inclusion: { in: [1,2,3,4,5] }
+  validates :acidity, inclusion: { in: [1,2,3,4,5] }
+
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
