895760d586e7493d2e801de2f45a8a05914df2b5
Sa110-5325 <sa110.35325@gmail.com>
2021-03-05 11:33:35 +0000

    [add]チャットデザイン

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,8 +13,8 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :follow
   has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverse_of_relationships, source: :user
-  has_many :user_rooms
-  has_many :chats
+  has_many :user_rooms, dependent: :destroy
+  has_many :chats, dependent: :destroy
   has_many :rooms, through: :user_rooms
 
   validates :name, presence: true
