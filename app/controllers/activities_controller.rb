e6aacb1b17810888bd24a88f6e518b775ac4080b
Sa110-5325 <sa110.35325@gmail.com>
2021-03-09 05:15:34 +0000

    [add]アクティビティ通知2

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,6 +2,9 @@ class ActivitiesController < ApplicationController
 
   def index
     @activities = current_user.active_notifications.page(params[:page]).per(10)
+    @activities.where(checked: false).each do |activity|
+      activity.update_attributes(checked: true)
+    end
     # @user = User.all
     # @comment = current_user.comments.all
     # @favorite = current_user.favorites.all
--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -1,2 +1,6 @@
 module ActivitiesHelper
+  def unchecked_activities
+    @activities = current_user.active_notifications.where(checked: false)
+  end
+  
 end
