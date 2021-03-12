4de3ced69ea63c712ff5c37949e0dab5808011c5
Sa110-5325 <sa110.35325@gmail.com>
2021-03-12 08:02:46 +0000

    [fix]トップページ3

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,6 +3,7 @@ class HomesController < ApplicationController
   layout 'index', except: [:about]
 
   def top
+    @rankings = Bean.find(Favorite.group(:bean_id).order('count(bean_id) DESC').limit(6).pluck(:bean_id))
   end
 
   def about
